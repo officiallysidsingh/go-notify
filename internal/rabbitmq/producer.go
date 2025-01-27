37d3daa065f47d0ced429c73333384dee83a8405
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-01-27 21:19:51 +0530

    Added rabbitmq producer code

---
+++ internal/rabbitmq/producer.go
@@ -0,0 +1,60 @@
+package rabbitmq
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQProducer struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	queue   amqp.Queue
+}
+
+// Init RabbitMQ Producer
+func NewProducer(url, queueName string) (*RabbitMQProducer, error) {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to RabbitMQ: %w", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open a channel: %w", err)
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to declare a queue: %w", err)
+	}
+
+	return &RabbitMQProducer{conn, ch, q}, nil
+}
+
+// To send a message to the queue
+func (p *RabbitMQProducer) Publish(message string) error {
+	return p.channel.Publish(
+		"",
+		p.queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+}
+
+// To shut down connection
+func (p *RabbitMQProducer) Close() {
+	p.channel.Close()
+	p.conn.Close()
+}
