3bc18490ed47c31baaab0e56c450db23f5010be4
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-02-08 03:33:57 +0530

    Added function to update status of notification

--- internal/db/db.go
+++ internal/db/db.go
@@ -20,15 +20,24 @@ func NewDB(dataSource string) *DB {
 	return &DB{Conn: conn}
 }
 
-func (d *DB) InsertNotification(userID, message, status string) error {
-	query := `INSERT INTO notifications (user_id, message, status) VALUES ($1, $2, $3)`
+func (d *DB) InsertNotification(userID, message, status string) (int64, error) {
+	var id int64
+	query := "INSERT INTO notifications (user_id, message, status) VALUES ($1, $2, $3) RETURNING id"
 
-	_, err := d.Conn.Exec(
+	err := d.Conn.QueryRow(
 		query,
 		userID,
 		message,
 		status,
-	)
+	).Scan(&id)
+
+	return id, err
+}
+
+func (d *DB) UpdateNotificationStatus(id int64, status string) error {
+	query := "UPDATE notifications SET status=$1 WHERE id=$2"
+
+	_, err := d.Conn.Exec(query, status, id)
 
 	return err
 }
