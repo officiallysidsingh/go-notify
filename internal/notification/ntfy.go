00ee87323d7dc157aadc19274ef5d19e470b93ec
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-02-08 03:46:35 +0530

    Created new package to send push notification using ntfy

---
+++ internal/notification/ntfy.go
@@ -0,0 +1,37 @@
+package notification
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+// SendPushNotification sends a push notification using ntfy.
+// - topic: your ntfy topic
+// - title: title for the notification
+// - message: the notification body
+func SendPushNotification(topic, title, message string) error {
+	url := fmt.Sprintf("https://ntfy.sh/%s", topic)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(message)))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Title", title)
+	req.Header.Set("Content-Type", "text/plain")
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusAccepted {
+		return fmt.Errorf("ailed to send push notification, status code: %d", res.StatusCode)
+	}
+
+	return nil
+}
