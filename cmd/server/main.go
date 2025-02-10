d9435cf7be179e20c8b92ebad2f3a0c27e4cfce2
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-02-11 00:25:13 +0530

    Added prometheus and also zap for logging and metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net"
+	"net/http"
 
 	"github.com/officiallysidsingh/go-notify/config"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 
 	pb "github.com/officiallysidsingh/go-notify/api/generated"
 	grpcserver "github.com/officiallysidsingh/go-notify/internal/grpc"
@@ -20,34 +21,51 @@ func main() {
 	// Load configuration from the config folder.
 	config.LoadConfig("./config")
 
+	// Structured logging
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	// Start Prometheus metrics HTTP server in separate goroutine
+	go func() {
+		sugar.Infof("Starting metrics server on %s", config.AppConfig.Metrics.Port)
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(config.AppConfig.Metrics.Port, nil); err != nil {
+			sugar.Fatalf("Metrics HTTP server failed: %v", err)
+		}
+	}()
+
 	// Init RabbitMQ Producer
 	producer, err := rabbitmq.NewProducer(
 		config.AppConfig.RabbitMQ.URL,
 		config.AppConfig.RabbitMQ.Queue,
 	)
 	if err != nil {
-		log.Fatalf("Failed to initialize RabbitMQ: %v", err)
+		sugar.Fatalf("Failed to initialize RabbitMQ: %v", err)
 	}
 	defer producer.Close()
 
-	// Init DB Connection
+	// Connect to Postgres DB
 	database := repository.NewDB(config.AppConfig.Postgres.DSN)
 
 	// Start gRPC Server
 	listener, err := net.Listen("tcp", config.AppConfig.GRPC.Port)
 	if err != nil {
-		log.Fatalf("Failed to listen on port %s: %v", config.AppConfig.GRPC.Port, err)
+		sugar.Fatalf("Failed to listen on port %s: %v", config.AppConfig.GRPC.Port, err)
 	}
 
+	// Create gRPC server with integrated notification service
 	server := grpcserver.NewNotificationServer(producer, database)
-
 	grpcServer := grpc.NewServer()
 	pb.RegisterNotificationServiceServer(grpcServer, server)
 
 	reflection.Register(grpcServer)
 
-	fmt.Printf("gRPC server is running on port %s\n", config.AppConfig.GRPC.Port)
+	sugar.Infof("gRPC server running on %s", config.AppConfig.GRPC.Port)
 	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Failed to serve gRPC server: %v", err)
+		sugar.Fatalf("Failed to serve gRPC server: %v", err)
 	}
 }
