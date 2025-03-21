854547c494a43eac8c3142af91cbbf642b3458ca
officiallysidsingh <siddharthsingh2014@gmail.com>
2025-03-22 01:55:13 +0530

    Added logic for creating and routing messages to exchanges
    - Added Dead Letter Exchange(DLX) and Dead Letter Queue(DLQ).
    - Added Topic Exchange to send notification based on key, i.e., notification type.
    - Added Fanout Exchange for broadcasting notification to all queues.

--- internal/rabbitmq/producer.go
+++ internal/rabbitmq/producer.go
@@ -11,11 +11,10 @@ import (
 type RabbitMQProducer struct {
 	conn    *amqp.Connection
 	channel *amqp.Channel
-	queue   amqp.Queue
 }
 
 // Init RabbitMQ Producer
-func NewProducer(url, queueName string) (*RabbitMQProducer, error) {
+func NewProducer(url string) (*RabbitMQProducer, error) {
 	var conn *amqp.Connection
 	var err error
 
@@ -28,7 +27,6 @@ func NewProducer(url, queueName string) (*RabbitMQProducer, error) {
 		log.Printf("RabbitMQ connection failed. Retrying... (%d/5)", i+1)
 		time.Sleep(2 * time.Second)
 	}
-
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to RabbitMQ: %w", err)
 	}
@@ -38,34 +36,146 @@ func NewProducer(url, queueName string) (*RabbitMQProducer, error) {
 		return nil, fmt.Errorf("failed to open a channel: %w", err)
 	}
 
-	q, err := ch.QueueDeclare(
-		queueName,
-		true,  // Durable queue
-		false, // Do not auto-delete
+	return &RabbitMQProducer{
+		conn:    conn,
+		channel: ch,
+	}, nil
+}
+
+// Declare necessary exchanges and queues
+func (p *RabbitMQProducer) SetupExchangesAndQueues() error {
+	// Declaring Dead Letter Exchange (DLX)
+	if err := p.channel.ExchangeDeclare(
+		"dead_letter_exchange",
+		"direct",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	// Declaring Dead Letter Queue (DLQ)
+	_, err := p.channel.QueueDeclare(
+		"dead_letter_queue",
+		true,
+		false,
 		false,
 		false,
 		nil,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to declare a queue: %w", err)
+		return err
 	}
 
-	return &RabbitMQProducer{conn, ch, q}, nil
+	// Bind DLQ to DLX with fixed routing key
+	if err := p.channel.QueueBind(
+		"dead_letter_queue",
+		"dead_letter",
+		"dead_letter_exchange",
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	// Declaring Topic Exchange for Notification Type based routing
+	if err := p.channel.ExchangeDeclare(
+		"notification_exchange_topic",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	// Declaring Fanout Exchange for broadcasting to all channels
+	if err := p.channel.ExchangeDeclare(
+		"notification_exchange_fanout",
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	// Enable Dead Lettering
+	queueArgs := amqp.Table{
+		"x-dead-letter-exchange":    "dead_letter_exchange",
+		"x-dead-letter-routing-key": "dead_letter",
+	}
+
+	// Queues for each notification type
+	queues := []struct {
+		Name       string
+		BindingKey string
+	}{
+		{"queue_email", "email"},
+		{"queue_sms", "sms"},
+		{"queue_push", "push"},
+	}
+
+	for _, q := range queues {
+		// Declare each notification type queue
+		_, err := p.channel.QueueDeclare(
+			q.Name,
+			true,
+			false,
+			false,
+			false,
+			queueArgs,
+		)
+		if err != nil {
+			return err
+		}
+
+		// Bind queue to topic exchange
+		if err := p.channel.QueueBind(
+			q.Name,
+			q.BindingKey,
+			"notification_exchange_topic",
+			false,
+			nil,
+		); err != nil {
+			return err
+		}
+
+		// Bind queue to fanout exchange
+		if err := p.channel.QueueBind(
+			q.Name,
+			"",
+			"notification_exchange_fanout",
+			false,
+			nil,
+		); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // To send a message to the queue
-func (p *RabbitMQProducer) Publish(message string) error {
+func (p *RabbitMQProducer) Publish(exchange, routingKey, message string) error {
 	var err error
 
 	// Retry upto 3 times
 	for i := 0; i < 3; i++ {
 		err = p.channel.Publish(
-			"",
-			p.queue.Name,
+			exchange,
+			routingKey,
 			false,
 			false,
 			amqp.Publishing{
-				ContentType:  "text/plain",
+				ContentType:  "application/json",
 				Body:         []byte(message),
 				DeliveryMode: amqp.Persistent,
 			},
@@ -85,6 +195,10 @@ func (p *RabbitMQProducer) Publish(message string) error {
 
 // To shut down connection
 func (p *RabbitMQProducer) Close() {
-	p.channel.Close()
-	p.conn.Close()
+	if p.channel != nil {
+		p.channel.Close()
+	}
+	if p.conn != nil {
+		p.conn.Close()
+	}
 }
